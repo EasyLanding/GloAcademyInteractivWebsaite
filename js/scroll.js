6543a24b22ebe78f716b7bab41b003db6e84078a
EasyLanding <90074687+EasyLanding@users.noreply.github.com>
2022-03-21 21:19:29 +0300

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,25 @@
+const links = document.querySelectorAll('.menu-list__link')
+const btn = document.querySelector('.main__button')
+const linksButton = document.querySelector('.main__scroll')
+
+const allLinks = [...links, btn, linksButton]
+
+allLinks.forEach(link =>
+{
+    link.addEventListener('click', (event) =>
+    {
+        event.preventDefault()
+
+        const id = link.getAttribute('href').substring(1)
+        const section = document.getElementById(id)
+
+        if (section)
+        {
+            seamless.scrollIntoView(section, {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+        }
+    })
+})
